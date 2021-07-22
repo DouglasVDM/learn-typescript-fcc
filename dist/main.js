289a4ef279025f755750198b05b155ae55a7db6e
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 17:39:46 +0200

    (as)typecast to htmlinputelement which has a value

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+var someElememt = document.querySelector(".foo");
+// Typescript does not See our Markup(HTML)
+console.log('someElememt', someElememt.value);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
+const someElememt = document.querySelector(".foo") as HTMLInputElement;
 
+// Typescript does not See our Markup(HTML)
+console.log('someElememt', someElememt.value)
