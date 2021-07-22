94734ffbf40bdeca221af59250fb9bc4e7588503
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 16:04:04 +0200

    can only use type string not array

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ const jello = "world"; //  Jello Type is "world" and Cannot be Changed because i
 let hello = "world"; //  Hello type is "String" because it's a Let, which can be Changed
 
 hello = true; //  Cannot use Boolean because Type is String
+hello = []; //  Cannot use Array because Type is String
 
 let kello: string = "world"; //  Best Practice is to assign Type Explicitly
