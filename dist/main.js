2e32032bfdde1bed6bb40f85074d318eac73a510
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 17:00:11 +0200

    type of any

--- dist/main.js
+++ dist/main.js
@@ -2,3 +2,8 @@
 var doSomething = function () {
     console.log("doSomething");
 };
+/*
+Any Turns Off TypeScript
+Any is the Start of Bigger problems
+Max 5 Any's for big Projects
+*/ 
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,9 @@
 const doSomething = (): void => {
   console.log("doSomething")
 }
+
+/* 
+Any Turns Off TypeScript
+Any is the Start of Bigger problems
+Max 5 Any's for big Projects
+*/
