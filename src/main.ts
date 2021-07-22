da5d1f3b58b1f107f919b939e570c92068aafea3
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 17:26:18 +0200

    07.working with the dom

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,1 @@
-let vAny: any = 10;
-let vUnknown: unknown = 10;
 
-let s1: string = vAny;
-let s2: string = vUnknown as string;
-
-let pageNumber: string = "1";
-let numericPageNumber: number = (pageNumber as unknown) as number;
-//  This is Type Assertion, To change Type, First change it to Unknown then to the Type you want.
-
-/*
-  Stick to Only to Type of Unknown and Void
-  Ignore usning Any in any case
-*/
