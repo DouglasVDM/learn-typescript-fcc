afd9eb74a94c02c61a93b53a0f3fc605edd3d540
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 16:13:17 +0200

    functions in typescript-specify type 2b returned

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,4 @@
-var jello = "world"; //  Jello Type is "world" and Cannot be Changed because it's a Constant
-var hello = "world"; //  Hello type is "String" because it's a Let, which can be Changed
-hello = true; //  Cannot use Boolean because Type is String
-hello = []; //  Cannot use Array because Type is String
-var kello = "world"; //  Best Practice is to assign Type Explicitly
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName('Monster', 'Ball'));
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-const jello = "world"; //  Jello Type is "world" and Cannot be Changed because it's a Constant
+const getFullName = (name: string, surname: string): string => {
+  return name + " " + surname;
+};
 
-let hello = "world"; //  Hello type is "String" because it's a Let, which can be Changed
-
-hello = true; //  Cannot use Boolean because Type is String
-hello = []; //  Cannot use Array because Type is String
-
-let kello: string = "world"; //  Best Practice is to assign Type Explicitly
+console.log(getFullName('Monster', 'Ball'));
