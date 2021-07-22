1e2990473739b98c0764d2381fafa6a4dd931b3f
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 16:01:14 +0200

    type definitions - create variables

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,4 @@
-var a = "1";
-console.log("aaa", a.foo());
+var jello = "world"; //  Jello Type is "world" and Cannot be Changed because it's a Constant
+var hello = "world"; //  Hello type is "String" because it's a Let, which can be Changed
+hello = true; //  Cannot use Boolean because Type is String
+var kello = "world"; //  Best Practice is to assign Type Explicitly
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
-const a = "1";
-console.log("aaa", a.foo());
+const jello = "world"; //  Jello Type is "world" and Cannot be Changed because it's a Constant
+
+let hello = "world"; //  Hello type is "String" because it's a Let, which can be Changed
+
+hello = true; //  Cannot use Boolean because Type is String
+
+let kello: string = "world"; //  Best Practice is to assign Type Explicitly
