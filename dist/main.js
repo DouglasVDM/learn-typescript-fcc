ba104187653ae475662bc3cc1e4e9e73e5c21dee
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-22 17:52:35 +0200

    08.classes

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-var someElememt = document.querySelector(".foo");
-// Using Typescript with DOM, just use "as" Operator to say what Type you have
-someElememt.addEventListener('blur', function (event) {
-    var target = event.target;
-    console.log('event', target.value);
-});
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-const someElememt = document.querySelector(".foo");
-
-// Using Typescript with DOM, just use "as" Operator to say what Type you have
-someElememt.addEventListener('blur', (event) => {
-  const target = event.target as HTMLInputElement;
-  console.log('event', target.value)
-});
