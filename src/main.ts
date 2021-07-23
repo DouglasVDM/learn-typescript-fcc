2cef930c62cfd3992cf3f8f0a1e5dcdc92d1a49a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-23 21:49:37 +0200

    Static properties are accessible only on class itself

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ interface UserInterface{
   firstName: string;
   lastName: string;
   readonly unchangableName: string; //  Read-only property
+  static maxAge = 50 
 
   constructor(firstnName: string, lastName: string) {
     this.firstName = firstnName;
@@ -16,10 +17,11 @@ interface UserInterface{
     // this.unchangableName = "foo";
   }
 
-  // getFullname(): string {
-  //   return this.firstName + " " + this.lastName;
-  // }
+  getFullname(): string {
+    return this.firstName + " " + this.lastName;
+  }
 }
 
 const user = new User("Monster", "lessons");
 console.log(user.firstName);
+console.log(User.maxAge); //  Static properties are accessible only on class itself
