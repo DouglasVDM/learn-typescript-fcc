9c9da3052a97e107cbfb873725e09b9c772c8252
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-23 22:04:34 +0200

    inheritance-child inherits from parent

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,27 @@ interface UserInterface{
   }
 }
 
+class Admin extends User{
+  private editor: string
+
+  setEditor(editor: string): void {
+    this.editor = editor
+  }
+
+  getEditor(): string {
+    return this.editor
+   }
+}
+
 const user = new User("Monster", "lessons");
 console.log(user.firstName);
-console.log(User.maxAge); //  Static properties are accessible only on class itself
+console.log(User.maxAge);
+
+const admin = new Admin('Foo', 'Bar');
+console.log(admin.changeUnchangableName());
+console.log(admin.firstName);
+console.log(admin.getEditor());
+console.log(admin.getFullname());
+console.log(admin.lastName);
+console.log(admin.setEditor('x'));
+console.log(admin.unchangableName());
